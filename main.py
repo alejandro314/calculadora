ebbb0e12bc7cd82443a586366c64fbdf460f199f
alejandro314 <manuelmartines385@gmail.com>
2025-04-29 20:00:14 -0500

    clase de html y flask 29/03/25

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from flask import Flask, request
-from operaciones import sumar
+from flask import Flask, request, render_template
+from operaciones import sumar, restar , multiplicar , division , 
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return '''
-    
-    
-    pagina de inicio
-    Calculadora
-
-    <a href="/suma?num1=5&num2=10"> Ir a sumar  </a>
-
-    '''
-
+    return render_template(home.html)
 @app.route("/suma")
 def ruta_sumar():
     num1=request.args.get("num1", type=float)
@@ -25,6 +16,36 @@ def ruta_sumar():
 
 
     return f"el resultado de la suma es {sumar(num1,num2)}"
+@app.route("/restar")
+def ruta_restar():
+    num1=request.args.get("num1", type=float)
+    num2=request.args.get("num2", type=float)
+
+    if num1 is None or num2 is None:
+        return "faltan datos para realizar la operacion"
+
+
+    return f"el resultado de la resta es {restar(num1,num2)}"
+@app.route("/multiplicar")
+def ruta_multiplicar():
+    num1=request.args.get("num1", type=float)
+    num2=request.args.get("num2", type=float)
+
+    if num1 is None or num2 is None:
+        return "faltan datos para realizar la operacion"
+
+
+    return f"el resultado de la multiplicacion  es {multiplicar(num1,num2)}"
+@app.route("/division")
+def ruta_division():
+    num1=request.args.get("num1", type=float)
+    num2=request.args.get("num2", type=float)
+
+    if num1 is None or num2 is None:
+        return "faltan datos para realizar la operacion"
+
+
+    return f"el resultado de la division  es {division(num1,num2)}"
 
 if __name__=="__main__":
     app.run(debug=True)
--- operaciones.py
+++ operaciones.py
@@ -1,2 +1,8 @@
 def sumar(a,b):
     return a+b
+def restar (a,b):
+    return a-b
+def multiplicar(a,b):
+    return a*b
+def division(a,b):
+    return a/b
