be0174f9a8c4e5b4df694a3c03cb18f1c1e5db0e
alejandro314 <manuelmartines385@gmail.com>
2025-05-08 20:08:13 -0500

    operaciones 875/2025

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template 
 from operaciones import sumar, restar , multiplicar , division 
 
 app=Flask(__name__)
